08dfe1b856008dc45ecb7f50250460023f0592ee
Pedro Pinheiro <pedropinheiro.dev@gmail.com>
2022-11-02 05:36:02 -0300

    feature: adding Config.kt

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,12 @@
+object Config {
+    const val NAMESPACE = "com.pedro.hearthstoneapplication"
+    const val COMPILE_SDK_VERSION = 33
+    const val MIN_SDK_VERSION = 29
+    const val TARGET_SDK_VERSION = 33
+    const val APPLICATION_ID = "com.pedro.hearthstoneapplication"
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+    const val TEST_INSTRUMENTATION_RUNNER = "1.0"
+    const val JVM_TARGET = "1.8"
+    const val KOTLIN_COMPILER_EXTENSION_VERSION = "1.2.0"
+}
