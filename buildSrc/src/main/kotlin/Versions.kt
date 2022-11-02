d63ef106ac05ce601891bca5da45efd419fd876f
Pedro Pinheiro <pedropinheiro.dev@gmail.com>
2022-11-02 05:36:46 -0300

    feature: adding Versions.kt

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    const val CORE_KTX_VERSION = "1.9.0"
+    const val LIFECYCLE_RUNTIME_VERSION = "2.5.1"
+    const val ACTIVITY_COMPOSE_VERSION = "1.6.1"
+    const val COMPOSE_UI_VERSION = "1.2.0"
+    const val COMPOSE_MATERIAL_VERSION = "1.3.0"
+    const val JUNT_VERSION = "4.13.2"
+    const val TEST_JUNIT_VERSION = "1.1.3"
+    const val ESPRESSO_VERSION = "3.4.0"
+}
