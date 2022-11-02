34c9b13270d97080bacbec0eeb24ba6f86b4dfbb
Pedro Pinheiro <pedropinheiro.dev@gmail.com>
2022-11-02 06:22:52 -0300

    lint: cleaning MainActivity.kt

--- app/src/main/java/com/pedro/hearthstoneapplication/MainActivity.kt
+++ app/src/main/java/com/pedro/hearthstoneapplication/MainActivity.kt
@@ -3,12 +3,7 @@ package com.pedro.hearthstoneapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pedro.designsystem.theme.HearthstoneApplicationTheme
 import com.pedro.onboarding.SplashScreen
@@ -24,15 +19,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HearthstoneApplicationTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
